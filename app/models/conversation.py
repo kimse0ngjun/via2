66e991e91752c90867b1a9e7934d71bee1cac32d
kimse0ngjun <tjdwns8756@naver.com>
2025-03-21 20:34:52 +0900

    +

--- app/models/conversation.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime, timezone
-
-# 대화 모델
-class ConversationCreate(BaseModel):
-    user_id: str
-    topic: str
-    created_at: datetime = datetime.now(timezone.utc)
-    is_deleted: bool = False
